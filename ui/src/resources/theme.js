f69a54f73d572ddb62ca94ff2a29e2d455616d78
menezesryan <menezesryan17@gmail.com>
2023-05-30 22:53:08 +0530

    Added Theme file

--- ui/src/resources/theme.js
+++ ui/src/resources/theme.js
@@ -1,1 +1,11 @@
 //contains the theme colors for elements and backgrounds
+
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+    typography: {
+        h1: {
+            fontSize: 10
+        }
+    }
+})
